bb72ca3dd8affc6886b45c8a3b6c80fec64fe3b8
torabekova <torabekovafarida6@gmail.com>
2024-10-09 17:50:08 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <img
+        src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg"
+        alt="rasm"
+      />
+      <p>Qurvaqa</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "./App.css";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <div className="container">
+      <App />
+      <App />
+      <App />
+      <App />
+      <App />
+      <App />
+      <App />
+      <App />
+      <App />
+      <App />
+      <App />
+      <App />
+    </div>
+  </React.StrictMode>
+);
+
+reportWebVitals();
